fb36773ddd3f1f27868efbdbce39c5120a1c4894
Alexandre Arnt <qtgzmanager@gmail.com>
2022-06-23 12:46:33 -0300

    BugFix: Disable Info/Files tab refresh while typing in filter/search line edit (AGAIN...)

--- src/mainwindow_refresh.cpp
+++ src/mainwindow_refresh.cpp
@@ -1322,6 +1322,10 @@ void MainWindow::refreshTabFiles(bool clearContents, bool neverQuit)
  */
 void MainWindow::reapplyPackageFilter()
 {
+  clearTabsInfoOrFiles();
+  disconnect(ui->tvPackages->selectionModel(), &QItemSelectionModel::selectionChanged,
+            this, &MainWindow::invalidateTabs);
+
   if (m_actionSwitchToRemoteSearch->isChecked())
   {
     if (m_leFilterPackage->text() == "")
@@ -1361,7 +1365,7 @@ void MainWindow::reapplyPackageFilter()
     QModelIndex mi = m_packageModel->index(0, PackageModel::ctn_PACKAGE_NAME_COLUMN, QModelIndex());
     ui->tvPackages->setCurrentIndex(mi);
     ui->tvPackages->scrollTo(mi);
-    clearTabsInfoOrFiles();
+    //clearTabsInfoOrFiles();
   }
   //If we are using "Search By file...
   else
@@ -1376,6 +1380,9 @@ void MainWindow::reapplyPackageFilter()
 
     connect(m_leFilterPackage, SIGNAL(textChanged(QString)), this, SLOT(reapplyPackageFilter()));
   }
+
+  connect(ui->tvPackages->selectionModel(), &QItemSelectionModel::selectionChanged,
+            this, &MainWindow::invalidateTabs);
 }
 
 /*
