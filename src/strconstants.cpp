3b8d21b6efeecb2a588de3c56165af2eae81de80
Alexandre Arnt <qtgzmanager@gmail.com>
2023-09-08 10:59:45 -0300

    - Preparing for release.

--- src/strconstants.cpp
+++ src/strconstants.cpp
@@ -27,7 +27,7 @@ QString StrConstants::getApplicationName(){
 }
 
 QString StrConstants::getApplicationVersion(){
-  return "0.3.3 (dev)";
+  return "0.3.3";
 }
 
 QString StrConstants::getQtVersion(){
